a8d6df432156e3b8a118fc5f3cf7ee559531f160
hoastust2k7 <5b3g0036@stust.edu.tw>
2025-11-03 15:13:43 +0800

    backup2

--- c-cpp1027/1.c
+++ c-cpp1027/1.c
@@ -20,37 +20,37 @@ int main(int argc, char *argv[]) {
         printf("\n輸入有誤！限 1～10 間的整數！\n");
     }
 
-    input = 0, a = 0, b = 0, c = 0, d = 0, sum = 0;
-    printf("第二次請使用者輸入數值(1~10)：");
-    scanf("%d", &input);
-    printf("\n輸入的數值 n = %d\n", input);
-    if (input >= 1 && input <= 10) {
-        printf("各項的乘積為：\n");
-        for (a = 1, b = 2, c = 3, d = 4; a <= input, b <= input * 2, c <= input * 3, d <= input * 4; a++, b += 2, c += 3, d += 4) {
-            sum += (a + b + c + d);
-            printf("(%d + %d + %d + %d) + ", a, b, c, d);
-        }
-        printf("\b\b\b = %d\n\n", sum);
-    }
-    else {
-        printf("\n輸入有誤！限 1～10 間的整數！\n");
-    }
+    // input = 0, a = 0, b = 0, c = 0, d = 0, sum = 0;
+    // printf("第二次請使用者輸入數值(1~10)：");
+    // scanf("%d", &input);
+    // printf("\n輸入的數值 n = %d\n", input);
+    // if (input >= 1 && input <= 10) {
+    //     printf("各項的乘積為：\n");
+    //     for (a = 1, b = 2, c = 3, d = 4; a <= input, b <= input * 2, c <= input * 3, d <= input * 4; a++, b += 2, c += 3, d += 4) {
+    //         sum += (a + b + c + d);
+    //         printf("(%d + %d + %d + %d) + ", a, b, c, d);
+    //     }
+    //     printf("\b\b\b = %d\n\n", sum);
+    // }
+    // else {
+    //     printf("\n輸入有誤！限 1～10 間的整數！\n");
+    // }
 
-    input = 0, a = 0, b = 0, c = 0, d = 0, sum = 0;
-    printf("第三次請使用者輸入數值(1~10)：");
-    scanf("%d", &input);
-    printf("\n輸入的數值 n = %d\n", input);
-    if (input >= 1 && input <= 10) {
-        printf("各項的乘積為：\n");
-        for (a = 1, b = 2, c = 3, d = 4; a <= input, b <= input * 2, c <= input * 3, d <= input * 4; a++, b += 2, c += 3, d += 4) {
-            sum += (a + b + c + d);
-            printf("各項的乘積為：\n");
-            printf("(%d + %d + %d + %d) + ", a, b, c, d);
-        }
-        printf("\b\b\b = %d\n\n", sum);
-    }
-    else {
-        printf("\n輸入有誤！限 1～10 間的整數！\n");
-    }
+    // input = 0, a = 0, b = 0, c = 0, d = 0, sum = 0;
+    // printf("第三次請使用者輸入數值(1~10)：");
+    // scanf("%d", &input);
+    // printf("\n輸入的數值 n = %d\n", input);
+    // if (input >= 1 && input <= 10) {
+    //     printf("各項的乘積為：\n");
+    //     for (a = 1, b = 2, c = 3, d = 4; a <= input, b <= input * 2, c <= input * 3, d <= input * 4; a++, b += 2, c += 3, d += 4) {
+    //         sum += (a + b + c + d);
+    //         printf("各項的乘積為：\n");
+    //         printf("(%d + %d + %d + %d) + ", a, b, c, d);
+    //     }
+    //     printf("\b\b\b = %d\n\n", sum);
+    // }
+    // else {
+    //     printf("\n輸入有誤！限 1～10 間的整數！\n");
+    // }
     return 0;
 }
---
+++ c-cpp1103-test/3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int input, first = 3, second = 2, third = 1, i, sum = 0;
+
+    printf("請輸入 X 值 ( 1~100 ) ：");
+    scanf("%d", &input);
+    printf("輸入的 X 值 = %d\n", input);
+    if(input >= 1 && input <= 100) {
+        printf("你計算的總和是：\n");
+        for(i = 1; i <= input; i++) {
+            printf("(%d + %d + 1) + ", i * i * first, i * second);
+            sum += 3*i*i + 2*i + 1;
+        }
+        printf("\b\b\b = %d\n\n", sum);
+    }
+    else {
+        printf("\n輸入有誤！限 1～100 間的整數！\n");
+    }
+
+    input, first = 3, second = 2, third = 1, i, sum = 0;
+    printf("請輸入 X 值 ( 1~100 ) ：");
+    scanf("%d", &input);
+    printf("輸入的 X 值 = %d\n", input);
+    if(input >= 1 && input <= 100) {
+        printf("你計算的總和是：\n");
+        for(i = 1; i <= input; i++) {
+            printf("(%d + %d + 1) + ", i * i * first, i * second);
+            sum += 3*i*i + 2*i + 1;
+        }
+        printf("\b\b\b = %d\n\n", sum);
+    }
+    else {
+        printf("\n輸入有誤！限 1～100 間的整數！\n");
+    }
+
+    input, first = 3, second = 2, third = 1, i, sum = 0;
+    printf("請輸入 X 值 ( 1~100 ) ：");
+    scanf("%d", &input);
+    printf("輸入的 X 值 = %d\n", input);
+    if(input >= 1 && input <= 100) {
+        printf("你計算的總和是：\n");
+        for(i = 1; i <= input; i++) {
+            printf("(%d + %d + 1) + ", i * i * first, i * second);
+            sum += 3*i*i + 2*i + 1;
+        }
+        printf("\b\b\b = %d\n\n", sum);
+    }
+    else {
+        printf("\n輸入有誤！限 1～100 間的整數！\n");
+    }
+
+    return 0;
+}
